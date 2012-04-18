78f8a1015c0e1aeb43f6443715cbf138f63d7099
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-04-18 12:09:28 +0100

    Getting ready for release

--- Gemfile
+++ Gemfile
@@ -11,5 +11,5 @@ group :development do
   gem "minitest", ">= 0"
   gem "bundler", "~> 1.0.0"
   gem "jeweler", "~> 1.6.4"
-  gem "rcov", ">= 0"
+  gem "simplecov", :require => false
 end
--- test/helper.rb
+++ test/helper.rb
@@ -13,6 +13,9 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'diffrent'
 
+require 'simplecov'
+SimpleCov.start
+
 class MiniTest::Unit::TestCase
 end
 
